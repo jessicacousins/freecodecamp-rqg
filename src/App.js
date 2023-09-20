bc031955f6775297ddc04d7ec90fc6aa9ffba150
Jessica Cousins <113946660+jessicacousins@users.noreply.github.com>
2023-09-19 23:21:47 -0400

    working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const getNewQuote = async () => {
+    try {
+      const response = await fetch("https://api.quotable.io/random");
+      const data = await response.json();
+      setQuote(data.content);
+      setAuthor(data.author);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    // Fetch initial quote on component mount
+    getNewQuote();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="container">
+      <div className="quote-box" id="quote-box">
+        <div className="text" id="text">
+          <p>{quote}</p>
+        </div>
+        <div className="author" id="author">
+          <p>- {author}</p>
+        </div>
+        <button id="new-quote" onClick={getNewQuote}>
+          New Quote
+        </button>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          id="tweet-quote"
+          href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+            `"${quote}" - ${author}`
+          )}`}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Tweet Quote
         </a>
-      </header>
+      </div>
     </div>
   );
 }
